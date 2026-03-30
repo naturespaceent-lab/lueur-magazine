a375005c1fa84c3c4f4928f8b9bda6e2f436a3c7
Noo <Noo@MacBook-Pro-2.local>
2026-03-31 04:35:36 +0900

    chore: clean orphaned images (save disk space)

--- crawl.mjs
+++ crawl.mjs
@@ -1091,7 +1091,8 @@ function escapeHtml(str) {
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;');
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
 
 // ============================================================
